84ff1489e79a2111c57edb10df82e172740cf937
Srinu <lakshmisrinivasulu970423@gmail.com>
2026-08-19 20:20:47 +0530

    i am uploding varible python code

---
+++ varibles.py
@@ -0,0 +1,26 @@
+name = "rotte vs srini"
+print(name)
+
+
+age = 20
+print(age)
+
+
+a = 10
+b = 20
+print(a + b)
+
+
+length = 10
+width = 5
+area = length * width
+print(area)
+
+
+a = 10
+b = 20
+
+a, b = b, a
+
+print(a)
+print(b)
