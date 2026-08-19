af3bcedf55fccf7fd40ac86054873e30b520cd44
Srinu <lakshmisrinivasulu970423@gmail.com>
2026-08-19 20:05:25 +0530

    i am uploding python code

---
+++ filehandling.py
@@ -0,0 +1,5 @@
+'''
+name = input("Enter your name:")
+age = input("Enter your age:")
+print("my name is:",  name,  "how old are u:",  age)
+'''
---
+++ srinu.py
@@ -0,0 +1,13 @@
+
+# int_value = int(input("Enter an int:"))
+# float_value = float(input("Enter a decimal num:"))
+# text_value = input("Enter some text:")
+# char_value = input("Enter a char:")
+
+# print("int", int_value)
+# print("float", float_value)
+# print("text", text_value)
+# print("char", char_value)
+
+
+
